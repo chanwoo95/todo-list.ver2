1745dcee4416203c5ded4b3dcd7d8674af21fbc1
cksdndbd95 <cksdndbd95@gmail.com>
2021-06-16 20:56:51 +0900

    add item

--- main.js
+++ main.js
@@ -14,6 +14,42 @@ setInterval(timer, 1000);
 
 // 2.아이템을 입력하면 리스트 목록에 추가된다.
 
+const input = document.querySelector('.footer__input');
+const button = document.querySelector('.footer__button');
+const container = document.querySelector('.item__container');
+
+button.addEventListener('click', onAdd);
+input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        onAdd();
+    }
+});
+
+function onAdd() {
+    const text = input.value;
+    console.log(text);
+    if (text === '') {
+        input.focus();
+        return;
+    }
+
+    createItem(text);
+
+    function createItem(text) {
+        const item = document.createElement('div');
+        item.setAttribute('class', 'item');
+        const button = document.createElement('button');
+        delBtn.setAttribute('class', 'item__button');
+        item.appendChild(delBtn);
+        item.textContent = text;
+
+        container.appendChild(item);
+    }
+
+    input.value = '';
+    input.focus();
+}
+
 // 3. 아이템 삭제 버튼을 누르면 리스트에 해당 아이템이 삭제된다.
 
 // 4. 총 아이템 개수를 표시해준다.
